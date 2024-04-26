b693aed844849a644e1ec9c3e762b4e3970ce60c
vljn <121915880+vljn@users.noreply.github.com>
2024-04-26 04:57:14 +0200

    Napravljena komponenta za naslov

---
+++ app/ui/title.jsx
@@ -0,0 +1,5 @@
+import { Glockenspiel } from './fonts/fonts';
+
+export default function Title() {
+  return <h1 className={`text-5xl text-center text-primary py-5 font-title`}>Knight Travails</h1>;
+}
