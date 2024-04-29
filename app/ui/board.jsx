f888cad3a64b4d0afc8709358d2c59e512dadedd
vljn <121915880+vljn@users.noreply.github.com>
2024-04-29 04:26:50 +0200

    Izmenjena Board komponenta

--- app/ui/board.jsx
+++ app/ui/board.jsx
@@ -1,21 +1,28 @@
-import Square from './square';
+import React from 'react';
+import Square from './Square';
 
 export default function Board() {
-  function generateRow(color) {
+  const renderRow = (rowIndex) => {
     const cells = [];
     for (let i = 0; i < 8; i++) {
-      cells.push(<Square color={color} />);
-      color = color === 'bg-primary' ? 'bg-secondary' : 'bg-primary';
+      cells.push(
+        <Square key={i} color={(rowIndex + i) % 2 === 0 ? 'bg-primary' : 'bg-secondary'} />
+      );
     }
-    return <div className="flex">{cells}</div>;
-  }
+    return cells;
+  };
 
-  const rows = [];
-  let color = 'bg-primary';
-  for (let i = 0; i < 8; i++) {
-    rows.push(generateRow(color));
-    color = color === 'bg-primary' ? 'bg-secondary' : 'bg-primary';
-  }
+  const renderBoard = () => {
+    const rows = [];
+    for (let i = 0; i < 8; i++) {
+      rows.push(
+        <div key={i} className="flex">
+          {renderRow(i)}
+        </div>
+      );
+    }
+    return rows;
+  };
 
-  return <div className="border-2 border-primary w-fit h-fit">{rows}</div>;
+  return <div className="border-2 border-primary w-fit h-fit">{renderBoard()}</div>;
 }
