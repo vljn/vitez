9c9485e2835a567bc4fb74ce7dc281f8b7bd3659
vljn <121915880+vljn@users.noreply.github.com>
2024-04-30 18:42:58 +0200

    Dodat link ga login strani

--- app/page.jsx
+++ app/page.jsx
@@ -2,17 +2,29 @@ import Title from './ui/title';
 import Button from './ui/button';
 import Board from './ui/board';
 import AllSquares from './ui/allSquares';
+import Link from 'next/link';
+import { ArrowRightIcon } from '@heroicons/react/24/outline';
+import { UserCircleIcon } from '@heroicons/react/24/solid';
 
 export default function Home() {
   return (
     <main className="flex flex-col lg:flex-row items-center lg:justify-between lg:h-svh lg:w-full lg:px-24 lg:gap-8 text-primary xl:gap-24">
-      <div className="max-lg:mb-2">
+      <div className="max-lg:mb-2 sm:text-xl lg:text-base xl:text-lg">
         <Title>Витез</Title>
-        <p className="text-center px-6 lg:px-0 sm:text-xl lg:text-base xl:text-lg">
+        <Link href="/login" className="lg:hidden text-knight-primary absolute top-7 left-8">
+          <UserCircleIcon className="w-8" />
+        </Link>
+        <p className="px-6 max-lg:text-center lg:px-0">
           Опште је познато да је коњ фигура која може доћи до било ког поља на табли, независно од
           поља са ког креће, као и да може посетити свако поље на табли, а да не посети исто поље
           два пута
         </p>
+        <Link
+          href="/login"
+          className="max-lg:hidden border-2 border-primary rounded text-primary hover:bg-primary hover:text-knight-white transition-colors mt-10 flex justify-between items-center w-36 p-2"
+        >
+          <span>Улогуј се</span> <ArrowRightIcon className="w-6" />
+        </Link>
       </div>
       <AllSquares />
     </main>
