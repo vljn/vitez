9ccd67694df1c523592c8963d71cab6a6dcd7739
vljn <121915880+vljn@users.noreply.github.com>
2024-04-30 17:42:36 +0200

    Promenjena margina

--- app/page.jsx
+++ app/page.jsx
@@ -6,7 +6,7 @@ import AllSquares from './ui/allSquares';
 export default function Home() {
   return (
     <main className="flex flex-col lg:flex-row items-center lg:justify-between lg:h-svh lg:w-full lg:px-24 lg:gap-8 text-primary xl:gap-24">
-      <div className="max-lg:mb-6">
+      <div className="max-lg:mb-2">
         <Title>Витез</Title>
         <p className="text-center px-6 lg:px-0 sm:text-xl lg:text-base xl:text-lg">
           Опште је познато да је коњ фигура која може доћи до било ког поља на табли, независно од
