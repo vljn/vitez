3b15bc20ff8562bcd7176a0f19b2d7f0db5d9275
vljn <121915880+vljn@users.noreply.github.com>
2024-04-30 17:48:20 +0200

    Popravljen bug sa bojom polja

--- app/ui/square.jsx
+++ app/ui/square.jsx
@@ -3,7 +3,7 @@ import { XMarkIcon } from '@heroicons/react/24/outline';
 export default function Square({ color, visited }) {
   return (
     <div
-      className={`square w-10 h-10 md:w-14 md:h-14 lg:w-16 lg:h-16 bg-${color} text-center ${
+      className={`square ${'bg-' + color} w-10 h-10 md:w-14 md:h-14 lg:w-16 lg:h-16 text-center ${
         color === 'primary' ? 'text-secondary' : 'text-primary'
       } flex items-center justify-center text-lg lg:text-2xl`}
     >
