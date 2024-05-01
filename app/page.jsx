554667f1783a6c34378a1aa511bb68b0b4a670db
vljn <121915880+vljn@users.noreply.github.com>
2024-05-01 03:00:27 +0200

    Menjanje stila naslova i početne strane

--- app/page.jsx
+++ app/page.jsx
@@ -9,21 +9,21 @@ import { UserCircleIcon } from '@heroicons/react/24/solid';
 export default function Home() {
   return (
     <main className="flex flex-col lg:flex-row items-center lg:justify-between lg:h-svh lg:w-full lg:px-24 lg:gap-8 text-primary xl:gap-24">
-      <div className="max-lg:mb-2 sm:text-xl lg:text-base xl:text-lg max-lg:text-center">
+      <div className="max-lg:mb-2 sm:text-xl lg:text-base xl:text-2xl max-lg:text-center lg:flex lg:justify-center lg:items-center lg:flex-col">
         <Title>Витез</Title>
         <Link href="/login" className="lg:hidden absolute top-7 left-8">
           <UserCircleIcon className="w-8" />
         </Link>
-        <p className="px-6 max-lg:text-center lg:px-0">
+        <p className="px-6 lg:px-0 text-center xl:mt-6">
           Опште је познато да је коњ фигура која може доћи до било ког поља на табли, независно од
           поља са ког креће, као и да може посетити свако поље на табли, а да не посети исто поље
           два пута
         </p>
         <Link
           href="/login"
-          className="max-lg:hidden border-2 border-primary rounded hover:bg-primary hover:text-knight-white transition-colors mt-10 flex justify-between items-center w-36 p-2"
+          className="max-lg:hidden border-2 border-primary rounded hover:bg-primary hover:text-knight-white transition-colors lg:mt-10 xl:mt-20 flex justify-between items-center lg:w-40 xl:w-56 p-4"
         >
-          <span>Улогуј се</span> <ArrowRightIcon className="w-6" />
+          <span>Улогуј се</span> <ArrowRightIcon className="w-6 xl:w-8" />
         </Link>
       </div>
       <AllSquares />
