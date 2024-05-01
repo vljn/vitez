fe214dee7f95c8e80be60f9e6e241f3de19578d8
vljn <121915880+vljn@users.noreply.github.com>
2024-05-01 02:12:15 +0200

    Dodat hover efekat na naslovu

--- app/ui/title.jsx
+++ app/ui/title.jsx
@@ -2,7 +2,9 @@ import { notoSerifDisplay } from './fonts/fonts';
 
 export default function Title({ children }) {
   return (
-    <h1 className={`text-5xl my-5 lg:my-0 md:text-6xl md:mb-3 xl:text-8xl font-title lg:mb-4`}>
+    <h1
+      className={`text-5xl mt-5 mb-3 lg:my-0 md:text-6xl xl:text-8xl font-title drop-shadow-2xl select-none lg:hover:scale-105 lg:transition-transform lg:w-min`}
+    >
       {children}
     </h1>
   );
