6794116932ef6efc76691f00e6db5a0677124583
vljn <121915880+vljn@users.noreply.github.com>
2024-05-02 22:26:36 +0200

    Promenjen stil slider-a za brzinu

--- app/ui/allSquares.jsx
+++ app/ui/allSquares.jsx
@@ -92,7 +92,7 @@ export default function AllSquares() {
           id="brzina"
           disabled={isRunning}
           className="flex-1 ml-4 h-2 bg-secondary border-2 border-primary appearance-none rounded
-          [&::-webkit-slider-thumb]:appearance-none 
+          [&::-webkit-slider-thumb]:appearance-none
           [&::-webkit-slider-thumb]:!bg-secondary
           [&::-webkit-slider-thumb]:w-4
           [&::-webkit-slider-thumb]:h-4
@@ -100,11 +100,13 @@ export default function AllSquares() {
           [&::-webkit-slider-thumb]:border-4
           [&::-webkit-slider-thumb]:border-solid
           [&::-webkit-slider-thumb]:border-primary
-          [&::-webkit-slider-thumb]:disabled:!bg-gray-500
-          [&::-webkit-slider-thumb]:hover:scale-125
           [&::-webkit-slider-thumb]:transition-transform
+          hover:[&::-webkit-slider-thumb]:scale-125
+          cursor-pointer
           disabled:bg-gray-500
-          cursor-pointer"
+          [&::-webkit-slider-thumb]:disabled:!bg-gray-500
+          hover:[&::-webkit-slider-thumb]:disabled:scale-100
+          disabled:cursor-default"
         />
       </div>
     </div>
