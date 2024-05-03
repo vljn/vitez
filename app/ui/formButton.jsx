feaadcf40f0419eb3bd2e1a235c516897f35e889
vljn <121915880+vljn@users.noreply.github.com>
2024-05-03 22:54:57 +0200

    Dodata mogućnost pozadine na dugme forme

--- app/ui/formButton.jsx
+++ app/ui/formButton.jsx
@@ -3,14 +3,16 @@
 import { ArrowPathIcon } from '@heroicons/react/24/solid';
 import { useFormStatus } from 'react-dom';
 
-export default function FormButton({ children }) {
+export default function FormButton({ children, background }) {
   const { pending } = useFormStatus();
 
   return (
     <button
       disabled={pending}
       type="submit"
-      className={`border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:bg-primary hover:text-knight-white transition-all w-52 flex justify-center items-center`}
+      className={`${
+        background && 'bg-secondary'
+      } border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:bg-primary hover:text-knight-white transition-all w-52 flex justify-center items-center`}
     >
       {pending ? <ArrowPathIcon className="w-7 animate-spin" /> : children}
     </button>
