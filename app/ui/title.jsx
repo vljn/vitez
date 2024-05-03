cb4ddb0ff3a7ea5b39267ccabaa2081213f4f288
vljn <121915880+vljn@users.noreply.github.com>
2024-05-03 04:59:38 +0200

    Izbačena margina sa naslova

--- app/ui/title.jsx
+++ app/ui/title.jsx
@@ -3,7 +3,7 @@ import { notoSerifDisplay } from './fonts/fonts';
 export default function Title({ children }) {
   return (
     <h1
-      className={`text-5xl mt-5 mb-3 lg:my-0 md:text-6xl xl:text-9xl font-title drop-shadow-2xl select-none lg:hover:scale-105 lg:transition-transform lg:w-min`}
+      className={`text-5xl lg:my-0 md:text-6xl xl:text-9xl font-title drop-shadow-2xl select-none lg:hover:scale-105 lg:transition-transform lg:w-min relative z-90`}
     >
       {children}
     </h1>
