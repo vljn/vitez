ce3dcaf175c18dabd1228f01b321003776c5118a
vljn <121915880+vljn@users.noreply.github.com>
2024-05-03 16:29:54 +0200

    Promenjena pozadina dugmeta za pozadinu

--- app/ui/backgroundSwitcher.jsx
+++ app/ui/backgroundSwitcher.jsx
@@ -43,7 +43,7 @@ export default function BackgroundSwitcher() {
         checked={isOn}
         className="sr-only peer"
       />
-      <div className="relative w-11 h-6 bg-knight-white peer-checked:bg-backgroundImage peer-checked:bg-[length:300px_100px] rounded-full peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-primary after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
+      <div className="relative w-11 h-6 bg-background peer-checked:bg-backgroundImage peer-checked:bg-[length:300px_100px] rounded-full peer-checked:after:translate-x-full after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-primary after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
     </label>
   );
 }
