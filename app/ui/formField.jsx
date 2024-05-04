b2a80f296465eee9f4a98314c293f9050a4a6b8d
vljn <121915880+vljn@users.noreply.github.com>
2024-05-04 23:54:08 +0200

    Dodata mogućnost otkrivanja šifre

--- app/ui/formField.jsx
+++ app/ui/formField.jsx
@@ -1,17 +1,32 @@
+'use client';
+
+import { EyeIcon, EyeSlashIcon } from '@heroicons/react/24/solid';
 import FormError from './formError';
+import { useState } from 'react';
 
 export default function FormField({ label, type, name, id, error, last }) {
+  const [showPassword, setShowPassword] = useState(false);
+
   return (
-    <div className={`${last ? 'mb-8' : 'mb-4'}`}>
+    <div className={`${last ? 'mb-8' : 'mb-4'} relative`}>
       <label htmlFor="password" className="block font-bold text-sm mb-2 lg:text-lg">
         {label}
       </label>
       <input
-        type={type}
+        type={showPassword ? 'text' : type}
         name={name}
         id={id}
         className="appearance-none rounded bg-primary border-none text-knight-white focus: px-3 py-2 w-full leading-tight focus:ring-knight-white"
       />
+      {type === 'password' && (
+        <button
+          type="button"
+          onClick={() => setShowPassword(!showPassword)}
+          className="absolute right-2 bottom-2 text-knight-white w-5"
+        >
+          {showPassword ? <EyeSlashIcon /> : <EyeIcon className="" />}
+        </button>
+      )}
       {error && <FormError text={error} />}
     </div>
   );
