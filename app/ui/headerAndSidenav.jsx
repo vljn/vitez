e1bccc5470c396a709587f78d970cefed48d7388
vljn <121915880+vljn@users.noreply.github.com>
2024-05-04 19:49:36 +0200

    Promenjen način renderovanja login dugmeta

--- app/ui/headerAndSidenav.jsx
+++ app/ui/headerAndSidenav.jsx
@@ -15,7 +15,7 @@ export default function HeaderAndSidenav({ username, link }) {
       <header className="flex justify-between items-center px-8 py-4 lg:mb-10">
         <Title link={link}>Витез</Title>
         <div className="flex items-center gap-6 sm:gap-10">
-          {!username && <LoginButton />}
+          {username ? null : <LoginButton />}
           <MenuButton isShowing={isShowing} setIsShowing={setIsShowing} />
         </div>
       </header>
