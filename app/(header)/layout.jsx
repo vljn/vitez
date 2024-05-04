257994326b7ff223d4ec599506d275667150ac06
vljn <121915880+vljn@users.noreply.github.com>
2024-05-04 20:04:13 +0200

    Korisnik se izloguje kada se nalog sa njegovim id-em ne pronađe

--- app/(header)/layout.jsx
+++ app/(header)/layout.jsx
@@ -1,12 +1,16 @@
 import HeaderAndSidenav from '../ui/headerAndSidenav';
 import { auth } from '@/auth';
 import { getUser } from '../lib/data';
+import { signOut } from 'next-auth/react';
 
 export default async function Layout({ children }) {
   const session = await auth();
   let user = null;
   if (session?.user) {
     user = await getUser(session?.user.id);
+    if (!user) {
+      signOut();
+    }
     console.log('sesija: ', session);
     console.log('sesija korisnik: ', session?.user);
     console.log('getUser: ', user);
