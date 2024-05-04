0dffc7c88bd95ab85577e2df87d0b651900960be
vljn <121915880+vljn@users.noreply.github.com>
2024-05-04 20:56:45 +0200

    Dodata redirekcija posle signOut-a

--- app/ui/signOutClient.jsx
+++ app/ui/signOutClient.jsx
@@ -2,10 +2,14 @@
 
 import { signOut } from 'next-auth/react';
 import { useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function SignOutClient() {
+  const router = useRouter();
+
   useEffect(() => {
     signOut();
+    router.push('/login');
   });
 
   return;
