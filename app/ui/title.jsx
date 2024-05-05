65876dacf9690033a009b40ab1997e0f295fb496
vljn <121915880+vljn@users.noreply.github.com>
2024-05-05 04:57:06 +0200

    Popravljen naslov tako da bude link

--- app/ui/title.jsx
+++ app/ui/title.jsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
 
-export default function Title({ children }) {
+export default function Title({ children, link }) {
   return (
     <h1
-      className={`text-5xl lg:my-0 md:text-6xl xl:text-7xl font-title drop-shadow-2xl select-none lg:hover:scale-105 lg:transition-transform lg:w-min relative z-90`}
+      className={`text-5xl lg:my-0 md:text-6xl xl:text-7xl font-title drop-shadow-2xl select-none lg:hover:scale-105 lg:transition-transform lg:w-min relative z-90 hover:cursor-pointer`}
     >
-      {children}
+      <Link href="/">{children}</Link>
     </h1>
   );
 }
