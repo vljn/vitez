b83fcc24524110274a315141e05cba3f8917ccb7
vljn <121915880+vljn@users.noreply.github.com>
2024-05-07 04:36:31 +0200

    Middleware funkcija za autentikaciju proverava rute na kojima korisnik sme da bude

--- middleware.js
+++ middleware.js
@@ -5,7 +5,7 @@ export const config = {
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
 
-const protectedRoutes = ['/nalog'];
+const unProtectedRoutes = [];
 const authRoutes = ['/login', '/registracija'];
 
 const { auth } = NextAuth(authConfig);
@@ -14,14 +14,23 @@ export default auth((req) => {
   const { nextUrl } = req;
 
   const isAuthenticated = !!req.auth;
-  const isProtectedRoute = protectedRoutes.some((prefix) => nextUrl.pathname.startsWith(prefix));
+  const isOnHome = nextUrl.pathname === '/';
+  const isProtectedRoute = !unProtectedRoutes.some((prefix) => nextUrl.pathname.startsWith(prefix));
   const isAuthRoute = authRoutes.includes(nextUrl.pathname);
 
-  if (!isAuthenticated && isProtectedRoute) {
-    return Response.redirect(new URL('/login', nextUrl.origin));
+  if (isOnHome) {
+    return null;
+  }
+
+  if (!isAuthenticated && isAuthRoute) {
+    return null;
   }
 
   if (isAuthenticated && isAuthRoute) {
     return Response.redirect(new URL(nextUrl.origin));
+  }
+
+  if (!isAuthenticated && isProtectedRoute) {
+    return Response.redirect(new URL('/login', nextUrl.origin));
   }
 });
