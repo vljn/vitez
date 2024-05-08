a1a5d5b407e952ae2b3874f375e473b67c4acba3
vljn <121915880+vljn@users.noreply.github.com>
2024-05-09 00:12:10 +0200

    Izbačeni nepotrebni stilovi sa početne strane

--- app/(header)/page.jsx
+++ app/(header)/page.jsx
@@ -7,9 +7,9 @@ export default async function Home() {
 
   return (
     <>
-      <main className="flex flex-col lg:flex-row items-center lg:justify-between lg:w-full lg:px-24 lg:gap-8 text-primary xl:gap-24">
-        <div className="max-lg:mb-4 sm:text-xl lg:text-base xl:text-2xl max-lg:text-center lg:flex lg:justify-center lg:items-center lg:flex-col px-6 lg:px-0 text-center xl:mt-6">
-          <p>
+      <main className="flex flex-col lg:flex-row items-center lg:justify-between lg:w-full lg:px-24 lg:gap-8 xl:gap-24">
+        <article className="max-lg:mb-4 sm:text-xl lg:text-base xl:text-2xl max-lg:text-center px-6 lg:px-0 text-center">
+          <p className="mb-2">
             Опште је познато да је коњ фигура која може доћи до било ког поља на табли, независно од
             поља са ког креће, као и да може посетити свако поље на табли, а да не посети исто поље
             два пута
@@ -26,7 +26,7 @@ export default async function Home() {
               (За приступ целокупном садржају је потребно бити улогован)
             </p>
           )}
-        </div>
+        </article>
         <KnightsTour coordinates={false} />
       </main>
     </>
