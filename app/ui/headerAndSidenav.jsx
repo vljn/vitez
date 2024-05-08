b7b6a90e155897d39e0bf3fe71367b5a710bf58d
vljn <121915880+vljn@users.noreply.github.com>
2024-05-08 19:41:31 +0200

    Postavljena pozadina na header-u nakon skrolovanja

--- app/ui/headerAndSidenav.jsx
+++ app/ui/headerAndSidenav.jsx
@@ -2,16 +2,37 @@
 
 import Title from './title';
 import LoginButton from './loginButton';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import MenuButton from './menuButton';
 import Sidenav from './sidenav';
 
-export default function HeaderAndSidenav({ username, link }) {
+export default function HeaderAndSidenav({ username }) {
   const [isShowing, setIsShowing] = useState(false);
+  const [headerBackground, setHeaderBackground] = useState(false);
+
+  useEffect(() => {
+    const scrollEvent = () => {
+      if (window.scrollY >= 48) {
+        setHeaderBackground(true);
+      } else {
+        setHeaderBackground(false);
+      }
+    };
+
+    window.addEventListener('scroll', scrollEvent);
+
+    return () => {
+      window.removeEventListener('scroll', scrollEvent);
+    };
+  }, []);
 
   return (
     <>
-      <header className="flex justify-between items-center px-8 py-4 lg:mb-10">
+      <header
+        className={`box-border sticky top-0 flex justify-between items-center px-8 py-4 lg:mb-10 transition-colors border-primary ${
+          headerBackground && 'bg-secondary border-b-2'
+        }`}
+      >
         <Title>Витез</Title>
         <div className="flex items-center gap-6 sm:gap-10">
           {username ? null : <LoginButton />}
