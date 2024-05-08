b135e93e7bad8999cb130a6165b65b78717f0078
vljn <121915880+vljn@users.noreply.github.com>
2024-05-09 00:21:15 +0200

    Dodata nova stranica

--- app/ui/sidenav.jsx
+++ app/ui/sidenav.jsx
@@ -9,6 +9,7 @@ const pages = [
   { id: 2, link: '/problem_konjickog_skoka', label: 'Проблем коњичког скока' },
   { id: 3, link: '/konjicki-skok-izazov', label: 'Коњички скок: изазов' },
   { id: 4, link: '/najkraci-put', label: 'Најкраћи пут' },
+  { id: 5, link: '/najkraci-put-izazov', label: 'Најкраћи пут: изазов' },
 ];
 
 export default function Sidenav({ show, showCallback, username }) {
