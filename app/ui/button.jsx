9c33d746d1f55f043a6b6f4125af0578ed9652c5
vljn <121915880+vljn@users.noreply.github.com>
2024-05-12 17:16:15 +0200

    Popravljena pozadina na dugmetu

--- app/ui/button.jsx
+++ app/ui/button.jsx
@@ -2,9 +2,11 @@ export default function Button({ children, onClick, className, active, disabled 
   return (
     <button
       onClick={onClick}
-      className={`border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:bg-primary hover:text-knight-white transition-all bg-secondary ${className} ${
-        active && 'bg-primary text-knight-white'
-      } ${disabled && 'hover:cursor-not-allowed  bg-gray-600 hover:bg-gray-600 text-knight-white'}`}
+      className={`border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:bg-primary hover:text-knight-white transition-all ${
+        !active && !disabled && 'bg-secondary'
+      } ${className} ${active && 'bg-primary text-knight-white'} ${
+        disabled && 'hover:cursor-not-allowed  bg-gray-600 hover:bg-gray-600 text-knight-white'
+      }`}
     >
       {children}
     </button>
