660d6168fbcaecdd54ceb849233440b858ea6fcc
vljn <121915880+vljn@users.noreply.github.com>
2024-05-15 00:58:45 +0200

    Sklonjen hover efekat sa dugmeta kada je ugašeno

--- app/ui/button.jsx
+++ app/ui/button.jsx
@@ -2,9 +2,9 @@ export default function Button({ children, onClick, className, active, disabled 
   return (
     <button
       onClick={onClick}
-      className={`border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:bg-primary hover:text-knight-white transition-all ${
-        !active && !disabled && 'bg-secondary'
-      } ${className} ${active && 'bg-primary text-knight-white'} ${
+      className={`${className} border-primary border-2 px-2 py-1 text-base sm:text-lg rounded hover:text-knight-white transition-all ${
+        !active && !disabled && 'bg-secondary hover:bg-primary'
+      } ${active && 'bg-primary text-knight-white'} ${
         disabled && 'hover:cursor-not-allowed  bg-gray-600 hover:bg-gray-600 text-knight-white'
       }`}
     >
