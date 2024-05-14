94ebdc0061e482fc89b69d5867506e51e889d0ba
vljn <121915880+vljn@users.noreply.github.com>
2024-05-15 01:20:50 +0200

    Dodate funkcije za proveravanje da li je polje napadnutno

--- app/lib/knight.js
+++ app/lib/knight.js
@@ -1,10 +1,10 @@
-export function isValid(x, y, visited) {
+export function isValid(x, y, notAllowed) {
   const valid =
     x >= 0 &&
     x <= 7 &&
     y >= 0 &&
     y <= 7 &&
-    !visited.some((square) => square.x === x && square.y === y);
+    !notAllowed.some((square) => square.x === x && square.y === y);
   return valid;
 }
 
@@ -13,4 +13,17 @@ export function isInRange(x1, y1, x2, y2) {
     (Math.abs(x1 - x2) === 1 && Math.abs(y1 - y2) === 2) ||
     (Math.abs(x1 - x2) === 2 && Math.abs(y1 - y2) === 1)
   );
+}
+
+function isAttacked(x, y, figure) {
+  if (figure.figura === 'top') {
+    return x === figure.x || y === figure.y;
+  }
+  if (figure.figura === 'lovac') {
+    return Math.abs(figure.x - x) === Math.abs(figure.y - y);
+  }
+}
+
+export function isSquareAttacked(x, y, figures) {
+  return figures.some((figure) => isAttacked(x, y, figure));
 }
