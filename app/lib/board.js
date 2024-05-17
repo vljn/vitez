119a8b7a86728feaa3b3c178ea3b61fc4d4609b9
vljn <121915880+vljn@users.noreply.github.com>
2024-05-17 03:06:24 +0200

    Prerađena funkcija za najkraći put

--- app/lib/board.js
+++ app/lib/board.js
@@ -1,33 +1,26 @@
 import { isValid } from './knight';
 import { directions, countValidMoves } from './knight';
 
-function generateBoard() {
-  const arr = [...new Array(8)].map(() => Array(8).fill(''));
-  return arr;
-}
-
 export function knightMoves(x1, y1, x2, y2) {
-  const board = generateBoard();
-  const queue = [];
-  queue.push({ x: x1, y: y1, moves: [] });
+  const queue = [{ x: x1, y: y1, moves: [] }];
+  const visited = [];
+
   while (queue.length > 0) {
     const top = queue.shift();
-    const [x, y] = [top.x, top.y];
-    if (x >= 0 && x <= 7 && y >= 0 && y <= 7 && board[x][y] === '') {
-      const arr = [...top.moves];
-      arr.push({ x, y });
-      if (x === x2 && y === y2) {
-        return arr;
-      }
-      board[x][y] = '1';
-      const xMove = [2, 1, -1, -2, -2, -1, 1, 2];
-      const yMove = [1, 2, 2, 1, -1, -2, -2, -1];
-      for (let index = 0; index < 8; index += 1) {
-        const newX = x + xMove[index];
-        const newY = y + yMove[index];
-        queue.push({ x: newX, y: newY, moves: arr });
-      }
+    const { x, y, moves } = top;
+    visited.push({ x, y });
+
+    if (x === x2 && y === y2) {
+      return [...moves, { x, y }];
     }
+
+    directions
+      .map((d) => ({ x: x + d.dx, y: y + d.dy }))
+      .filter((move) => isValid(move.x, move.y, visited))
+      .forEach((move) => {
+        visited.push({ x: move.x, y: move.y });
+        queue.push({ x: move.x, y: move.y, moves: [...moves, { x, y }] });
+      });
   }
 
   return [];
