25476449a42cb8cd1f9b93094f1015ef6af25886
vljn <121915880+vljn@users.noreply.github.com>
2024-05-19 05:35:20 +0200

    Promenjen izgled dugmeta za menjanje datuma

--- app/ui/admin/datePicker.jsx
+++ app/ui/admin/datePicker.jsx
@@ -2,28 +2,24 @@ import { ArrowLeftIcon, ArrowRightIcon } from '@heroicons/react/24/outline';
 
 export default function DatePicker({ date, setDate }) {
   return (
-    <div className="bg-primary text-knight-white flex justify-between items-center w-56 m-auto rounded-xl">
-      <div className="border-r border-knight-white">
-        <button
-          className="m-2"
-          onClick={() => {
-            setDate(date.plus({ days: -1 }));
-          }}
-        >
-          <ArrowLeftIcon className="w-6" />
-        </button>
-      </div>
+    <div className="bg-primary text-knight-white flex justify-between items-center w-56 m-auto rounded-xl mt-10 lg:mt-0">
+      <button
+        className="px-2 border-r border-knight-white h-10"
+        onClick={() => {
+          setDate(date.plus({ days: -1 }));
+        }}
+      >
+        <ArrowLeftIcon className="w-6" />
+      </button>
       <span className="text-xl mx-2">{date.setLocale('sr-rs').toLocaleString()}</span>
-      <div className="border-l border-knight-white">
-        <button
-          className="m-2"
-          onClick={() => {
-            setDate(date.plus({ days: 1 }));
-          }}
-        >
-          <ArrowRightIcon className="w-6" />
-        </button>
-      </div>
+      <button
+        className="px-2 border-l border-knight-white h-10"
+        onClick={() => {
+          setDate(date.plus({ days: 1 }));
+        }}
+      >
+        <ArrowRightIcon className="w-6" />
+      </button>
     </div>
   );
 }
